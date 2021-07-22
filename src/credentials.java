abbca90ca6a0e14c55f1e18782294d87888f2a96
Aryan Dinakaran <42404746+AryanDinakaran1@users.noreply.github.com>
2021-07-23 00:11:20 +0530

    Update credentials.java

--- src/credentials.java
+++ src/credentials.java
@@ -2,5 +2,5 @@ public class credentials
 {
     String url = "jdbc:mysql://192.168.0.105:3306/studentsdb";
     String user = "attendee";
-    String pass = "SPACEx#2468";
+    String pass = DBPASSWD;
 }
